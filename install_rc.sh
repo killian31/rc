6a5980c89dd5c553216a1e855df76f4c9d8398f3
Killian Steunou <80256683+killian31@users.noreply.github.com>
2024-06-19 13:59:45 +0200

    Update install_rc.sh

--- install_rc.sh
+++ install_rc.sh
@@ -2,10 +2,9 @@
 
 SCRIPT_NAME="rc"
 SCRIPT_PATH="/usr/local/bin/$SCRIPT_NAME"
-SCRIPT_URL="https://raw.githubusercontent.com/killian/rc/main/rc.sh"
 
 if [ ! -f "$SCRIPT_PATH" ]; then
-  curl -o "$SCRIPT_PATH" "$SCRIPT_URL"
+  cp ./rc.sh "$SCRIPT_PATH"
   chmod +x "$SCRIPT_PATH"
   echo "$SCRIPT_NAME installed successfully!"
 else
